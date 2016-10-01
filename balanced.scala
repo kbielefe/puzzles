4566bd5a5ec0c917b18d152e90aedc03047967d8
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2016-10-01 02:58:29 -0500

    Remove repetition with count.

--- balanced.scala
+++ balanced.scala
@@ -1,8 +1,9 @@
 def balanced(input: String): Boolean = {
-  val balances = input.scanLeft(0){
-      case (count, '(') => count + 1
-      case (count, ')') => count - 1
-      case (count,  _ ) => count
+  val balances = input.scanLeft(0){(count, char) => char match {
+      case '(' => count + 1
+      case ')' => count - 1
+      case  _  => count
+    }
   }
   balances.last == 0 && !balances.contains(-1)
 }
